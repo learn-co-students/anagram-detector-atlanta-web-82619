4e6433c2cd02739d93225d2e4e705da8023e4467
Justina Kuchta <44618164+jwkuchta@users.noreply.github.com>
2019-11-11 16:31:22 -0500

    Update anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Anagram
 
-    attr_accessor :name
+    attr_accessor :word
   
     def initialize(word)
-      @name = word 
+      @word = word 
     end   
   
     def match(array)
-      array.select {|x| x.split("").sort == @name.split("").sort}
+      array.select {|el| el.split("").sort == @word.split("").sort}
     end   
   
   end 
